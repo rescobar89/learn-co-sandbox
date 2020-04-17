78f6bb39ca75d17327e9777acc07413f1e0d0223
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-17 04:54:11 +0000

    Done.

---
+++ Variable_Examples.rb
@@ -0,0 +1,6 @@
+# Open up IRD and follow along
+sound = "squeak"
+
+# We can peek at the value of sound by typing its name
+
+sound
---
+++ return_values.rb
@@ -0,0 +1,15 @@
+# evil_monster = "Bowser"
+ 
+# def princess_peaches_castle
+#   puts "#{evil_monster} is trying to kidnap Princess Peach!"
+# end
+
+# princess_peaches_castle
+
+name = "Joe"
+ 
+def greeting(name)
+   puts "Hello, #{name}"
+end
+
+greeting("Sophie")
