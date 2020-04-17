eda1c63be0cf5e3e79096d38fc7d138334f98f81
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-17 05:08:29 +0000

    Done.

--- return_values.rb
+++ return_values.rb
@@ -5,11 +5,23 @@
 # end
 
 # princess_peaches_castle
+# > NameError: undefined local variable or method `evil_monster' for main:Object
+# this is because the variable it is looking for is outside of the method scope so it will not work.
 
-name = "Joe"
+# name = "Joe"
  
-def greeting(name)
-   puts "Hello, #{name}"
-end
+# def greeting(name)
+#   puts "Hello, #{name}"
+# end
 
-greeting("Sophie")
+# greeting("Sophie")
+
+
+
+evil_monster = "Bowser"
+ 
+def princess_peaches_castle(evil_monster)
+  puts "#{evil_monster} is trying to kidnap Princess Peach!"
+end
+ 
+princess_peaches_castle(evil_monster)
