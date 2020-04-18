32cac2d87cdd9311c10127e4655d8f9378d238f0
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-18 07:08:45 +0000

    Done.

--- if_statements.rb
+++ if_statements.rb
@@ -1,5 +1,39 @@
 if 2 > 5
   puts "2 is greater than 5"
+# else
+#   puts "2 is not greater than 5"
+end
+
+if false
+  puts "this will never get printed because the above statement evaluates to false"
 else
-  puts "2 is not greater than 5"
+  puts "this will get printed"
+end
+
+if 6 + 3 == 9
+  puts "Giraffes have no vocal cords"
+end
+
+if 6 + 3 < 5
+  puts "the hummingbird is the only animal that can fly backwards"
+else
+  puts "this is not true"
+end
+
+
+--------------------------------------
+#in this scenerio we are ulsing elsif which is based on more than one condition. For example if i am hungry then i will get a snack, if i am thirsty, then i will get a drink of water. Otherwise I will stay put and read my book. it can add additional layers of complexity to our if and selse statements
+
+dog = "cuddly"
+
+if dog == "hungry"
+  puts "refilling food bowl"
+elsif dog == "thirsty"
+  puts "refilling water bowl"
+elsif dog == "playful"
+  puts "playing tug-o-war"
+elsif dog == "cuddly"
+  puts "snuggling"
+else
+  puts "reading newspaper"
 end
